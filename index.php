cbafc6aa5beaec54fe9f53ca8dd2796f3a818138
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-09-20 10:10:51 +0200

    Finito esercizio

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tommaso Parigi</h1>
+    <?php
+        $peso = 62; // peso in kg
+        $risultato = 62*30; // peso in kg * 30 ml
+        echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $risultato</p>"
+    ?>
+</body>
+</html>
